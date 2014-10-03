ec36eac1489313112e57e7e797a278eeceaa597b
John Heroy <johnheroy@gmail.com>
2014-10-02 21:36:05 -0700

    Parse link param in POST req

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ var Yodel = function(){
 Yodel.prototype.parseYo = function(req){
   var toUser = req.params.touser;
   var fromUser = req.query.username;
+  var link = req.query.link || null;
   console.log('* new Yo received *');
   console.log('to user', toUser);
   console.log('from user', fromUser);
-  return {'toUser': toUser, 'fromUser': fromUser};
+  if (link){ console.log('with link', link); }
+  return {
+    'toUser'  : toUser,
+    'fromUser': fromUser,
+    'link'    : link
+  };
 };
 
 module.exports = new Yodel();
